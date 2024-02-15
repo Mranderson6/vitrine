d4b007314b53715890945e18fcdae50ab3bc630f
Mranderson6 <bigshow.stephane@gmail.com>
2024-02-15 15:49:13 +0100

    ajout des observations

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#autocomplete', {
-    strings: ['Que rechercher vous?','Une information ?','Un service ?'],
+    strings: ['Que recherchez vous ?','Une information ?','Un service ?'],
     typeSpeed: 50,
     backSpeed: 20,
     backDelay: 1000,
